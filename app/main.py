0cb88f3f0f1be65b8f7ebafda784cba724176e29
yacineKHA <yacine.kad@outlook.fr>
2026-05-05 16:36:10 +0200

    update

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-from routers import auth
+from app.routers import auth
 
 app = FastAPI(
-    title=TruitR,
+    title="TruitR",
 )
 
 origins = ["*"]
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Form, HTTPException
 from utils.auth_utils import hash_password, verify_password
-router = APIRouter(tags=["Authentication"])
+from database.database import get_connection
 
-# Fake "base de données"
-fake_users_db = {}
+
+router = APIRouter(tags=["Authentication"])
 
 
 @router.post("/login")
@@ -11,9 +11,13 @@ def login_a_user(
     username: str = Form(...),
     password: str = Form(...)
 ):
-    user = fake_users_db.get(username)
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM user WHERE pseudo = ?", (username,))
+    user = cursor.fetchone()
+    conn.close()
 
-    if not user or not verify_password(password, user["password"]):
+    if not user or not verify_password(password, user["mot_de_passe"]):
         raise HTTPException(status_code=400, detail="Invalid credentials")
 
     return {"access_token": username, "token_type": "bearer"}
@@ -24,13 +28,19 @@ def registrate_a_user(
     username: str = Form(...),
     password: str = Form(...)
 ):
-    if username in fake_users_db:
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id FROM user WHERE pseudo = ?", (username,))
+    if cursor.fetchone():
+        conn.close()
         raise HTTPException(status_code=400, detail="User already exists")
 
     hashed_password = hash_password(password)
-
-    fake_users_db[username] = {
-        "password": hashed_password
-    }
+    cursor.execute(
+        "INSERT INTO user (pseudo, mot_de_passe) VALUES (?, ?)",
+        (username, hashed_password)
+    )
+    conn.commit()
+    conn.close()
 
     return {"message": "User created successfully"}
