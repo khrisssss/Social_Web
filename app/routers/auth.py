2b3172bc25acf614489d0fe0f3c92ff4af1edaa5
yacineKHA <yacine.kad@outlook.fr>
2026-05-06 10:56:20 +0200

    update

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,46 +1,45 @@
 from fastapi import APIRouter, Form, HTTPException
+from pydantic import BaseModel
 from utils.auth_utils import hash_password, verify_password
 from database.database import get_connection
 
 
 router = APIRouter(tags=["Authentication"])
 
+class UserModel(BaseModel):
+    username: str
+    password: str
+
 
 @router.post("/login")
-def login_a_user(
-    username: str = Form(...),
-    password: str = Form(...)
-):
+def login_a_user(userConnect: UserModel):
     conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM user WHERE pseudo = ?", (username,))
+    cursor.execute("SELECT * FROM user WHERE pseudo = ?", (userConnect.username,))
     user = cursor.fetchone()
     conn.close()
 
-    if not user or not verify_password(password, user["mot_de_passe"]):
-        raise HTTPException(status_code=400, detail="Invalid credentials")
+    if not user or not verify_password(userConnect.password, user["mot_de_passe"]):
+        raise HTTPException(status_code=400, detail="Identification incorrect")
 
-    return {"access_token": username, "token_type": "bearer"}
+    return {"User connected ": user["pseudo"]}
 
 
 @router.post("/registration")
-def registrate_a_user(
-    username: str = Form(...),
-    password: str = Form(...)
-):
+def registrate_a_user(userConnect: UserModel):
     conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT id FROM user WHERE pseudo = ?", (username,))
+    cursor.execute("SELECT id FROM user WHERE pseudo = ?", (userConnect.username,))
     if cursor.fetchone():
         conn.close()
-        raise HTTPException(status_code=400, detail="User already exists")
+        raise HTTPException(status_code=400, detail="L'utilisateur existe déjà")
 
-    hashed_password = hash_password(password)
+    hashed_password = hash_password(userConnect.password)
     cursor.execute(
         "INSERT INTO user (pseudo, mot_de_passe) VALUES (?, ?)",
-        (username, hashed_password)
+        (userConnect.username, hashed_password)
     )
     conn.commit()
     conn.close()
 
-    return {"message": "User created successfully"}
+    return {"message": "User créé"}
