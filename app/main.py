353b26ee13d78b0493ff59f53c55912742512a57
yacineKHA <yacine.kad@outlook.fr>
2026-05-07 11:19:13 +0200

    deplace folders

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-from routers import auth, upload
+from .routers import auth, upload
 
 app = FastAPI(
     title="TruitR",
